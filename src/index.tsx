59818e84179f0daeb2f23505c52b567017cbb80b
ahriri <katielo522@gmail.com>
2022-03-02 00:53:14 -0800

    added wallet connector

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { getChainOptions, WalletProvider } from '@terra-money/wallet-provider';
 import { Anchor } from 'components/Anchor';
+import { Connect } from 'components/Connect';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
@@ -9,6 +11,7 @@ function App() {
     <main
       style={{ margin: 20, display: 'flex', flexDirection: 'column', gap: 40 }}
     >
+      <Connect />
       <Anchor />
     </main>
   );
